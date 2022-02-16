365eae0a7cc37f0352058cdeebe8fcf9f6da0309
allison <allison.villa@outlook.com>
2022-02-16 01:37:23 -0500

    added randomized quiz options, correctAnswer variable

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-// Quiz TO-DO
-
-// Create a form element that gets displayed/reset every round 
-// The form will be composed of radio inputs and a submit answer button
-// Variables for score and numRounds
-// Create a gameLogic method - compares user input to correct answer, adds points or not, increases round count
-
 const ghibliApp = {}
 
 ghibliApp.apiUrl = 'https://ghibliapi.herokuapp.com/films/'; 
 
-// Declare correctAnswer and userAnswer variables so they are accessible to be changed later
+// Declare quiz game variables so they are accessible to be changed later
 // Can maybe put these in the init later??
 ghibliApp.correctAnswer = '';
 ghibliApp.userAnswer = '';
+ghibliApp.numRounds = 0; 
 
 ghibliApp.init = function() {
     ghibliApp.getMovie();
@@ -25,18 +19,48 @@ ghibliApp.arrayRandomiser = function(array) {
 }
 
 ghibliApp.getMovie = function() {
-    fetch(ghibliApp.apiUrl)
+    const url = new URL(ghibliApp.apiUrl); 
+    url.search = new URLSearchParams({
+        // Only grabbing the fields we need
+        fields: 'title,description,movie_banner',
+        // I tried to increase this limit so we access more of the database (currently only returns same 22 movies) - doesn't work for some reason
+        limit: 100
+    });
+    fetch(url)
         .then(function(response) {
             return response.json();
         })
         .then(function(jsonResponse){
             console.log(jsonResponse);
-            // Call randomizer function to narrow results to one movie choice
-            ghibliApp.randomResult = ghibliApp.arrayRandomiser(jsonResponse);
-            ghibliApp.displayMovie(ghibliApp.randomResult); 
+            ghibliApp.gameSetup(jsonResponse);
         }); 
 }
 
+// Function which sets up the game 
+ghibliApp.gameSetup = function(apiData) {
+    // Select 4 random movies from the array using arrayRandomiser()
+    // There is currently a small chance it will choose the same movie twice 
+    const movieOne = ghibliApp.arrayRandomiser(apiData); 
+    const movieTwo = ghibliApp.arrayRandomiser(apiData); 
+    const movieThree = ghibliApp.arrayRandomiser(apiData); 
+    const correctMovie = ghibliApp.arrayRandomiser(apiData); 
+    // Store the titles of the above movie arrays into inputs and correctAnswer variable
+    // Currently the 4th option is always the right answer 🤔 Should figure out way to randomize this 
+    document.querySelector('#choice-1').value = movieOne.title; 
+    document.querySelector('#label-1').textContent = movieOne.title; 
+    document.querySelector('#choice-2').value = movieTwo.title; 
+    document.querySelector('#label-2').textContent = movieTwo.title;
+    document.querySelector('#choice-3').value = movieThree.title;
+    document.querySelector('#label-3').textContent = movieThree.title;
+    document.querySelector('#choice-4').value = correctMovie.title;
+    document.querySelector('#label-4').textContent = correctMovie.title;
+    ghibliApp.correctAnswer = correctMovie.title; 
+    // Display correct movie
+    ghibliApp.displayMovie(correctMovie); 
+    console.log(`The correct movie is ${correctMovie.title}`);
+}
+
+// Function which displays the correct movie description and image
 ghibliApp.displayMovie = function(apiData) {
     // Target the elements where movie data will be displayed
     ghibliApp.titleEl = document.querySelector('#movie-title');
@@ -55,7 +79,18 @@ ghibliApp.displayMovie = function(apiData) {
     ghibliApp.imgContainer.appendChild(image);
 }
 
+// Add an event listener which will call gameLogic() when submit button is clicked 
+
 ghibliApp.gameLogic = function() {
+    // Compare user input to correct answer
+
+    // Add points if applicable
+
+    // Increase numRounds count
+
+    // If numRounds < 4, call displayMovie() again to begin new round
+
+    // End game when numRounds == 4
 
 }
 
