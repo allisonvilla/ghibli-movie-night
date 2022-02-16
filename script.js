d86ae67071f4b37ea4fbc2f2beb95e96a6479d46
sylvia <sylviaraposo@hotmail.ca>
2022-02-16 17:08:37 -0500

    moving to a new branch

--- script.js
+++ script.js
@@ -2,84 +2,67 @@ const ghibliApp = {}
 
 ghibliApp.apiUrl = 'https://ghibliapi.herokuapp.com/films/';
 
-// Declare quiz game variables so they are accessible to be changed later
-// Can maybe put these in the init later??
-ghibliApp.correctAnswer = '';
-ghibliApp.userAnswer = '';
-ghibliApp.numRounds = 0;
-
-ghibliApp.init = function () {
+ghibliApp.init = function() {
     ghibliApp.getMovie();
     ghibliApp.quizEventListener();
 }
 
-// Function which accepts an array as an argument and returns a randomly chosen value from within that array
-ghibliApp.arrayRandomiser = function (array) {
-    const arrayIndex = Math.floor(Math.random() * array.length);
-    return array[arrayIndex];
+// Function which accepts an array as an argument and returns a random index
+ghibliApp.arrayRandomiser = function(array) {
+  const arrayIndex = Math.floor(Math.random() * array.length);
+  return array[arrayIndex]; 
 }
 
-ghibliApp.getMovie = function () {
-    const url = new URL(ghibliApp.apiUrl);
+ghibliApp.getMovie = function() {
+    const url = new URL(ghibliApp.apiUrl); 
     url.search = new URLSearchParams({
         // Only grabbing the fields we need
-        fields: 'title,description,movie_banner',
-        // I tried to increase this limit so we access more of the database (currently only returns same 22 movies) - doesn't work for some reason 🤔
-        limit: 100
+        fields: 'title,description,movie_banner'
     });
     fetch(url)
-        .then(function (response) {
+        .then(function(response) {
             return response.json();
         })
-        .then(function (jsonResponse) {
-            console.log(jsonResponse);
+        .then(function(jsonResponse){
             // Narrow the initial array (jsonResponse) into 4 random movies
             numberOfMovies = 4;
             const randomMovies = jsonResponse.sort(() => .5 - Math.random()).slice(0, numberOfMovies)
-
-            // console.log(randomMovies);
             ghibliApp.gameSetup(randomMovies);
-        });
+        }); 
 }
 
+// Declare quiz game variables so they are accessible across app scope
+ghibliApp.correctAnswer = '';
+ghibliApp.userScore = 0; 
+ghibliApp.numRounds = 0; 
+
 // Function which sets up the game 
-ghibliApp.gameSetup = function (apiData) {
-
+ghibliApp.gameSetup = function(apiData) {
     console.log(apiData);
-
-
-    // const currentAnswer = $(`input[name=${answerKey}]:checked`).val();
-
-    // assign the correct movie answer to a correctMovie variable    
-    const correctMovie = ghibliApp.arrayRandomiser(apiData);
-
+    // Display movie titles as options
     const choiceElement = document.querySelectorAll('.choice');
     const labelElement = document.querySelectorAll('.label');
-    // console.log(labelElement);
-
     choiceElement.forEach(choice => {
-        for (let i = 0; i < labelElement.length; i++) {
+        for(let i = 0; i < labelElement.length; i++) {
             choiceElement[i].value = apiData[i].title;
             labelElement[i].textContent = apiData[i].title;
-            console.log(i);
         }
     });
-
-    // Assign correct answer variable
-    ghibliApp.correctAnswer = correctMovie.title;
-
+    // Assign the correct movie answer to a correctMovie variable    
+    const correctMovie = ghibliApp.arrayRandomiser(apiData);
+    ghibliApp.correctAnswer = correctMovie.title; 
     // Display correct movie
-    ghibliApp.displayMovie(correctMovie);
+    ghibliApp.displayMovie(correctMovie); 
     console.log(`The correct movie is ${correctMovie.title}`);
-
 }
 
 // Function which displays the correct movie description and image
-ghibliApp.displayMovie = function (apiData) {
+ghibliApp.displayMovie = function(apiData) {
     // Target the elements where movie data will be displayed
     ghibliApp.titleEl = document.querySelector('#movie-title');
     ghibliApp.descEl = document.querySelector('#movie-description');
-    ghibliApp.imgContainer = document.querySelector('#movie-img');
+
+    ghibliApp.imgContainer = document.querySelector('#movie-img'); 
     // Store title and description from json response 
     movieTitle = apiData.title;
     movieDesc = apiData.description;
@@ -88,38 +71,58 @@ ghibliApp.displayMovie = function (apiData) {
     // Create an image element
     const image = document.createElement('img');
     image.src = apiData.movie_banner;
-    // Should probably change the alt text to not give away the answer lol
-    image.alt = `${apiData.title}'s movie banner`;
+    image.alt = `A Studio Ghibli movie banner`;
     // Append image element to div (ghibliApp.imgContainer)
     ghibliApp.imgContainer.appendChild(image);
 }
 
-// Add an event listener which will call gameLogic() when submit button is clicked 
-ghibliApp.quizEventListener = function () {
-    document.querySelector('#quiz-form').addEventListener('submit', function (event) {
+// Add an event listener which will call gameLogic() when submit button is clicked  
+ghibliApp.quizEventListener = function() {
+    document.querySelector('#quiz-form').addEventListener('submit', function(event) {
         event.preventDefault();
         ghibliApp.gameLogic();
-    });
+        const results = document.querySelector('.results');
+
+        results.style.display = 'flex';
+
+        // reset selection to the first option
+        const firstAnswer = document.querySelector('#choice-1');
+        firstAnswer.checked = true;
+        console.log(firstAnswer);
+    });    
 }
 
-ghibliApp.gameLogic = function () {
+
+ghibliApp.gameLogic = function() {
     console.log('gameLogic() is being called');
-    // store users input
-    //const userChoice = $(`input[name=${quiz}]:checked`).val();
-    let userChoice = document.querySelector('input:checked').value
-    
-
-    // Compare user input to correct answer
-    
-
-    // Add points if applicable
-
     // Increase numRounds count
+    ghibliApp.numRounds++; 
+    console.log(`Current number of rounds passed is ${ghibliApp.numRounds}`);
 
-    // If numRounds < 4, call gameSetup() again to begin new round
+    
 
-    // End game when numRounds == 4
-
+    // Store user input
+    let userInput = document.querySelector('input[name="quiz"]:checked').value; 
+    console.log(`User input was ${userInput}`);
+    // Compare user input to correct answer, increase score if necessary
+    if (userInput == ghibliApp.correctAnswer) {
+        ghibliApp.userScore++; 
+    } 
+    console.log(`User's current score is ${ghibliApp.userScore}`);
+    // If numRounds < 4, call getMovie() to get 4 new movies
+    if (ghibliApp.numRounds < 4) {
+        // Clear the movie img div
+        document.querySelector('#movie-img').innerHTML = '';
+        ghibliApp.getMovie(); 
+    // After 4 rounds, end the game
+    } else {
+        // Hide quiz div
+        document.querySelector('.quiz').style.display = 'none'; 
+        // Clear display
+        document.querySelector('.results').innerHTML = '';
+        // Show final score
+    }
 }
+
 
 ghibliApp.init(); 
